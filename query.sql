74e1a474118c11495df21600fe66969e455233bc
Ken_G <gonzalk2095@gmail.com>
2019-04-13 10:57:51 -0400

    Docuemtnation

--- query.sql
+++ query.sql
@@ -1,6 +1,13 @@
 -- Query to check successful load
+use GoT_db;
+
 SELECT * FROM ratios;
 
 SELECT * FROM members_Of_Each_House;
 
 SELECT * FROM members_alive;
+
+select character_name, allegiance
+from members_Of_Each_House
+order by allegiance ; 
+
